1ba70d96a6cca13382557678653344d96d346b0c
martin <martin.ashton@gmail.com>
2022-04-27 23:45:57 -0400

    Starting to implement source generation

---
+++ next/G3d/G3d.CodeGen/SourceGenerator.G3dSourceGenerator/G3d.g.cs
@@ -0,0 +1,11 @@
+﻿ // Auto-generated code
+using System;
+
+namespace G3d
+{
+    public static partial class G3d
+    {
+        static partial void HelloFrom(string name) =>
+            Console.WriteLine($"Generator says: Hi from '{name}'");
+    }
+}
